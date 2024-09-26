482d8fa1ed2a12f3fa710ceede3acb4ba93d5a63
Chi-Chidev <fabulousesther05@gmail.com>
2024-09-26 10:36:29 +0100

    stored my questions in an array in javascript

--- quiz.js
+++ quiz.js
@@ -1,1 +1,44 @@
-htm
+// put the questions into an array
+const quizQuestions = [
+    {
+        // i put the first question in a string
+        question: "Which planet is known as the Red Planet?",
+        // i will put the options each in a string, inside an array
+        options: ["Earth", "Mars", "Jupiter"],
+        // then i put the correct answer in a string aswell, this time not in an array cos its only one
+        answer: "Mars"
+    },
+    {
+        // i put the second question in a string
+        question: "What is the capital of France?",
+        // i will put the options each in a string, inside an array
+        options: ["Berlin", "Madrid", "Paris"],
+        // then i put the correct answer in a string aswell, this time not in an array cos its only one
+        answer: "Paris"
+    },
+    {
+        // i put the third question in a string
+        question: "What is the largest ocean on Earth?",
+        // i will put the options each in a string, inside an array
+        options: ["Atlantic Ocean", "Indian Ocean", "Pacific Ocean"],
+        // then i put the correct answer in a string aswell, this time not in an array cos its only one
+        answer: "Pacific Ocean"
+    },
+    {
+        // i put the fourth question in a string
+        question: "Who wrote the play 'Romeo and Juliet'?",
+        // i will put the options each in a string, inside an array
+        options: ["William Shakespeare", "Charles Dickens", "Mark Twain"],
+        // then i put the correct answer in a string aswell, this time not in an array cos its only one
+        answer: "William Shakespeare"
+    },
+
+    {
+        // i put the last question in a string
+        question: "What is the chemical symbol for water?",
+        // i will put the options each in a string, inside an array
+        options: ["O2", "H2O", "CO2"],
+        // then i put the correct answer in a string aswell, this time not in an array cos its only one
+        answer: "H2O"
+    },
+]
